68dda1934deda45ba4eb15a6876770fbbf0f77ab
Sibendu Dey <sibendudey@Sibendus-MacBook-Pro.local>
2018-12-11 18:41:26 -0500

    React routing added

--- voting-system-app/src/App.js
+++ voting-system-app/src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import CreatePollForm from './components/create_poll/CreatePollForm';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <CreatePollForm />
+        <Router>
+          <div>
+          <Route exact path="/" component={CreatePollForm}/>
+          <Route path="/create_poll" component={CreatePollForm} />
+          </div>
+        </Router>
       </div>
     );
   }
