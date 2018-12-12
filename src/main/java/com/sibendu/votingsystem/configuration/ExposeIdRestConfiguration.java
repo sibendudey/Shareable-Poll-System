0e51fe0094910b1591b90d63b038bed4e101007d
Sibendu Dey <sibendudey@Sibendus-MacBook-Pro.local>
2018-12-11 21:38:56 -0500

    Spring boot actuator with configuration for exposing id

---
+++ src/main/java/com/sibendu/votingsystem/configuration/ExposeIdRestConfiguration.java
@@ -0,0 +1,14 @@
+package com.sibendu.votingsystem.configuration;
+
+import com.sibendu.votingsystem.pojos.Poll;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExposeIdRestConfiguration extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Poll.class);
+    }
+}
